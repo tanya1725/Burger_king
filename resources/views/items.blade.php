fba7bdc07471edf5418e74b5cc6c56e0653d8d1d
tanya gupta <tanyagupta172510@gmail.com>
2024-08-15 14:27:55 +0530

    commit

---
+++ resources/views/items.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .background {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background: url('/image/secondwallpaper.jpg') no-repeat center center/cover;
+            opacity:40%;
+            z-index: -1;
+        }
+        .header
+        {
+            position: relative;
+            z-index:1;
+            color:white;
+            font-size: 80px;
+        }
+
+    </style>
+</head>
+<body bgcolor="black">
+    <div class="background"></div>
+    <div class="header">THE FRESHEST,MOST DELICIOUS <BR>BURGERS</div>
+</body>
+</html>
